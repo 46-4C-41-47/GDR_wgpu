bfa6b1e9a6887db2ae174f6b9b27047a4cc6274a
unknown <anzbevrctxyw192837465@gmail.com>
2024-12-17 13:39:49 +0100

    m

--- src/my_window.rs
+++ src/my_window.rs
@@ -1,24 +1,27 @@
-use wgpu::rwh::{HasRawWindowHandle, HasWindowHandle, RawWindowHandle};
+use std::sync::Arc;
 use winit::{
   application::ApplicationHandler, 
-  dpi::{ PhysicalSize, Size }, 
+  dpi::PhysicalSize, 
   event::{self, WindowEvent},
   keyboard::{KeyCode, PhysicalKey}
 };
-use crate::window_state::WindowSate;
+use crate::window_state::{self, WindowSate};
 
 
-pub struct MyWindow<'a> {
+pub struct MyWindow {
   title: String,
   size: winit::dpi::PhysicalSize<u32>,
   full_screen: bool,
 
-  window: Option<winit::window::Window>,
-  window_state: Option<WindowSate<'a>>,
+  window: Option<Arc<winit::window::Window>>,
+  window_state: Option<window_state::WindowSate>,
 }
 
 
-impl<'a> MyWindow<'a> {
+impl MyWindow {
+  const BACKGROUND_COLOR: wgpu::Color = wgpu::Color { r: 0.1, g: 0.1, b: 0.2, a: 1.0 };
+
+
   pub fn new(title: String, size: (u32, u32), full_screen:bool) -> Self {
     Self {
       title,
@@ -30,27 +33,51 @@ impl<'a> MyWindow<'a> {
   }
 
 
-  pub fn render(&self) -> Result<(), wgpu::SurfaceError> {
-    
-    Ok(())
+  fn toggle_fullscreen(&mut self) {
+    if self.full_screen {
+      self.window.as_ref().unwrap().set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
+    } else {
+      self.window.as_ref().unwrap().set_fullscreen(None);
+    }
+  }
+
+
+  fn handle_redraw(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+    self.window.as_ref().unwrap().request_redraw();
+        
+    match self.window_state.as_ref().unwrap().render(Self::BACKGROUND_COLOR) {
+      Ok(_) => {},
+
+      Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) 
+        => self.window_state.as_mut().unwrap().resize(self.size),
+        
+      Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+      
+      Err(wgpu::SurfaceError::OutOfMemory) => {
+        log::error!("OutOfMemory");
+        event_loop.exit();
+      }
+    }
   }
 }
 
 
-impl<'a> ApplicationHandler for MyWindow<'a> {
+impl ApplicationHandler for MyWindow {
   fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+    if self.window.is_some() { return; }
+
     let attrib = winit::window::Window::default_attributes()
       .with_title(self.title.clone())
       .with_inner_size(self.size.clone());
 
-    self.window = Some(event_loop.create_window(attrib).unwrap());
-    self.window_state = Some(WindowSate::new(self.window.as_ref().unwrap()));
+    let window: Arc<winit::window::Window> = Arc::new(
+      event_loop.create_window(attrib).unwrap()
+    );
 
-    if self.full_screen {
-      self.window.as_ref().unwrap().set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
-    }
+    self.window = Some(window.clone());
+    self.window_state = Some(WindowSate::new(window));
 
-    //let window_state = WindowSate::new(self.window.as_ref().unwrap());
+    self.toggle_fullscreen();
   }
   
 
@@ -60,9 +87,9 @@ impl<'a> ApplicationHandler for MyWindow<'a> {
     window_id: winit::window::WindowId,
     event: event::WindowEvent,
   ) {
-    if window_id != self.window.as_ref().unwrap().id() {
-      return;
-    }
+    if window_id != self.window.as_ref().unwrap().id() { return; }
+
+    if self.window_state.as_mut().unwrap().process_input(&event) { return; }
 
     match event {
       event::WindowEvent::CloseRequested | event::WindowEvent::KeyboardInput { 
@@ -72,15 +99,26 @@ impl<'a> ApplicationHandler for MyWindow<'a> {
           .. 
         }, 
         .. 
+      } => event_loop.exit(),
+
+      event::WindowEvent::KeyboardInput { 
+        event: event::KeyEvent { 
+          state: event::ElementState::Pressed, 
+          physical_key: PhysicalKey::Code(KeyCode::F1), 
+          .. 
+        }, 
+        .. 
       } => {
-        println!("exiting window");
-        event_loop.exit();
+        self.full_screen = !self.full_screen;
+        self.toggle_fullscreen();
       },
 
-      WindowEvent::RedrawRequested => {
-        self.window.as_ref().unwrap().request_redraw();
-        //self.render();
-      },
+      WindowEvent::Resized(physical_size) => {
+        log::info!("physical_size: {physical_size:?}");
+        self.window_state.as_mut().unwrap().resize(physical_size);
+      }
+
+      WindowEvent::RedrawRequested => self.handle_redraw(event_loop),
 
       _ => (),  
     }
--- src/window_state.rs
+++ src/window_state.rs
@@ -1,47 +1,149 @@
-use std::iter;
-use wgpu::{CommandEncoder, Surface, TextureFormat, TextureView};
+use std::{iter, sync::Arc};
+use wgpu::{Adapter, CommandEncoder, Surface, TextureFormat, TextureView};
 use winit::{
-  application::ApplicationHandler, 
   dpi::PhysicalSize, 
-  event::{self, WindowEvent}, 
-  event_loop::{self, EventLoop}, 
+  event::WindowEvent, 
   keyboard::{KeyCode, PhysicalKey}, 
-  window::{self, Window}
+  window::Window
 };
 use pollster::FutureExt as _;
 
 
-pub struct WindowSate<'a> {
-  surface: wgpu::Surface<'a>,
+pub struct WindowSate {
+  surface: wgpu::Surface<'static>,
   device: wgpu::Device,
   queue: wgpu::Queue,
   config: wgpu::SurfaceConfiguration,
   size: winit::dpi::PhysicalSize<u32>,
+  //window: Arc<Window>
 }
 
 
-impl<'a> WindowSate<'a> {
-  pub fn new<'b>(window: &'static Window) -> WindowSate<'b> {
-    todo!()
+impl WindowSate {
+  pub fn new(window: Arc<Window>) -> WindowSate {
+    let size: PhysicalSize<u32> = window.inner_size();
+
+    let instance: wgpu::Instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::PRIMARY,
+      ..Default::default()
+    });
+
+    let surface: Surface = instance.create_surface(window.clone()).unwrap();
+
+    let adapter: Adapter = instance
+      .request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::default(),
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+      }).block_on().unwrap();
+
+    let (device, queue) = Self::get_device_and_queue(&adapter);
+
+    let surface_caps: wgpu::SurfaceCapabilities = surface.get_capabilities(&adapter);
+    let surface_format: TextureFormat = surface_caps
+      .formats
+      .iter()
+      .copied()
+      .find(|f| f.is_srgb())
+      .unwrap_or(surface_caps.formats[0]);
+    let config: wgpu::SurfaceConfiguration = wgpu::SurfaceConfiguration {
+      usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+      format: surface_format,
+      width: size.width,
+      height: size.height,
+      present_mode: surface_caps.present_modes[0],
+      alpha_mode: surface_caps.alpha_modes[0],
+      desired_maximum_frame_latency: 2,
+      view_formats: vec![],
+    };
+
+    Self {
+      surface,
+      device,
+      queue,
+      config,
+      size,
+      //window
+    }
   }
 
 
-  fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-    todo!()
+  pub fn render(&self, default_bg_color: wgpu::Color) -> Result<(), wgpu::SurfaceError> {
+    let output = self.surface.get_current_texture()?;
+    let view = output
+      .texture
+      .create_view(&wgpu::TextureViewDescriptor::default());
+
+    let mut encoder = self
+      .device
+      .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Render Encoder"),
+      });
+
+    {
+      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: Some("Render Pass"),
+        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+          view: &view,
+          resolve_target: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(default_bg_color),
+            store: wgpu::StoreOp::Store,
+          },
+        })],
+        depth_stencil_attachment: None,
+        occlusion_query_set: None,
+        timestamp_writes: None,
+      });
+    }
+
+    self.queue.submit(iter::once(encoder.finish()));
+    output.present();
+
+    Ok(())
   }
 
 
-  fn input(&mut self, event: &WindowEvent) -> bool {
-    todo!()
-  }
-
-
-  fn update(&mut self) {
+  pub fn update(&mut self) {
     todo!()
   } 
 
 
-  fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-    todo!()
+  pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+    if new_size.width > 0 && new_size.height > 0 {
+      self.size = new_size;
+      self.config.width = new_size.width;
+      self.config.height = new_size.height;
+      self.surface.configure(&self.device, &self.config);
+    }
+  }
+
+
+  pub fn process_input(&mut self, event: &WindowEvent) -> bool {
+    match event {
+      // event::WindowEvent::KeyboardInput { 
+      //   event: event::KeyEvent { 
+      //     state: event::ElementState::Pressed, 
+      //     physical_key: PhysicalKey::Code(KeyCode::F1), 
+      //     .. 
+      //   }, 
+      //   .. 
+      // } => {},
+
+      _ => return false
+    }
+    return true;
+  }
+
+
+  fn get_device_and_queue(adapter: &Adapter) -> (wgpu::Device, wgpu::Queue) {
+    let device_descriptor: wgpu::DeviceDescriptor = wgpu::DeviceDescriptor {
+      label: None,
+          required_features: wgpu::Features::empty(),
+          required_limits: wgpu::Limits::default(),
+          memory_hints: Default::default(),
+    };
+
+    adapter.request_device(&device_descriptor, None).block_on().unwrap()
   }
 }
